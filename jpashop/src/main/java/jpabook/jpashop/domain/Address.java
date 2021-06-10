e1a7ffdc34c808203216aa78a165192d06082c5a
leeve1247 <multirkh@gmail.com>
2021-06-11 00:58:05 +0900

    Address. 생성자에 관련한 Lecture

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,16 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //JPA 특성상 만들어 놓았구나, 기본 생성자
+
+    protected Address(){
+    }
+
+    //생성할 때만 딱 값이 생성되고, 수정이 되어선 아니됩니다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
