861fc034c2835b68eaebe054e53d729658c73861
leeve1247 <multirkh@gmail.com>
2021-06-15 23:31:53 +0900

    MemberRepository 보완

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -4,42 +4,53 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+//형변환이나 LAZY를 위해서 Transactional을 사용
+@Transactional(readOnly = true)
 public class MemberService {
-    @Autowired
+
     private MemberRepository memberRepository;
 
+    //field only
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
     /**
-     *  회원가입
+     * 회원가입
      */
-    public Long join(Member member){
+    @Transactional
+    public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        List<Member> findMembers= memberRepository.findByName(member.getName());
-        if (!findMembers.isEmpty()){
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
 
     /**
-     *  회원 전체 조회
+     * 회원 전체 조회
      */
-    private List<Member> findMembers(){
+    //Transactional(readOnly = true)를 하면 데이터 변경이 아니됨
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
 
     /**
-     *  회원 ID로 하나 조회
+     * 회원 ID로 하나 조회
      */
-    private Member findOne(Long memberId){
+    public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
 }
